908cd3100d3ec26c314ddad2784a6157f1e19faf
Markus Staab <maggus.staab@googlemail.com>
2026-07-08 11:25:35 +0200

    nikic/PHP-Parser: re-use parser

--- nikic-PHP-Parser/bench.php
+++ nikic-PHP-Parser/bench.php
@@ -5,11 +5,13 @@ include_once __DIR__ . "/vendor/autoload.php";
 use PhpParser\Error;
 use PhpParser\NodeDumper;
 use PhpParser\ParserFactory;
+use PhpParser\Parser;
 
 $path = realpath(getcwd() . DIRECTORY_SEPARATOR . $argv[1]);
 
 $iterator = new RecursiveDirectoryIterator($path);
 $recursiveIterator = new RecursiveIteratorIterator($iterator);
+$parser = (new ParserFactory)->createForNewestSupportedVersion();
 
 /** @var SplFileInfo $path */
 foreach ($recursiveIterator as $path) {
@@ -22,13 +24,12 @@ foreach ($recursiveIterator as $path) {
         continue;
     }
     
-    parse($path);
+    parse($path, $parser);
     
     echo $path . "\n";
 }
 
-function parse(SplFileInfo $path) {
-    $parser = (new ParserFactory)->createForNewestSupportedVersion();
+function parse(SplFileInfo $path, Parser $parser) {
     try {
         $ast = $parser->parse(file_get_contents($path));
     } catch (Error $error) {

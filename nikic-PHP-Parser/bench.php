53c9f124541e7ba7e88d4b128306ae9b9e747e88
Theodore Brown <theodorejb@outlook.com>
2026-07-22 09:33:47 -0500

    Reuse single PHP Parser instance across all files
    Constructing the parser allocates hundreds of closures plus the token map,
    so rebuilding it per file is wasteful (and doesn't reflect normal usage).

--- nikic-PHP-Parser/bench.php
+++ nikic-PHP-Parser/bench.php
@@ -3,13 +3,14 @@
 include_once __DIR__ . "/vendor/autoload.php";
 
 use PhpParser\Error;
-use PhpParser\NodeDumper;
+use PhpParser\Parser;
 use PhpParser\ParserFactory;
 
 $path = realpath(getcwd() . DIRECTORY_SEPARATOR . $argv[1]);
 
 $iterator = new RecursiveDirectoryIterator($path);
 $recursiveIterator = new RecursiveIteratorIterator($iterator);
+$parser = (new ParserFactory)->createForNewestSupportedVersion();
 
 /** @var SplFileInfo $path */
 foreach ($recursiveIterator as $path) {
@@ -22,13 +23,12 @@ foreach ($recursiveIterator as $path) {
         continue;
     }
     
-    parse($path);
+    parse($parser, $path);
     
     echo $path . "\n";
 }
 
-function parse(SplFileInfo $path) {
-    $parser = (new ParserFactory)->createForNewestSupportedVersion();
+function parse(Parser $parser, SplFileInfo $path) {
     try {
         $ast = $parser->parse(file_get_contents($path));
     } catch (Error $error) {
